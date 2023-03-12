410e7fa0d2c61e4c0ce46c6469e06867cd4fb367
Borhan Lee <BorhanLee10@gmail.com>
2023-03-12 15:56:11 +0800

    finished Caesar and Permutation rubocop checked

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,11 +1,25 @@
+# frozen_string_literal: true
+
+# Validates credit card number using Luhn Algorithm
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
+  def calculate_sum(num_len, nums_a, weight, total)
+    (0..(num_len - 1)).each do |i|
+      result = (nums_a[num_len - i - 1].to_i * weight[i].to_i)
+      product = result >= 10 ? (result % 10 + (result / 10).floor) : result
+      total += product
+    end
+    total
+  end
+
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-
-    # TODO: use the integers in nums_a to validate its last check digit
+    num_len = nums_a.length
+    weight = 12_121_212_121_212_121_212.to_s.split('')
+    total = calculate_sum(num_len, nums_a, weight, 0)
+    (total % 10).zero? ? true : false
   end
 end
--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 module SubstitutionCipher
+  # creating Caesar cipher
   module Caesar
+    @uppercase_letters = ('A'..'Z').to_a # Uses the range to create the uppercase letters and converts it to an array
+    @lowercase_letters = ('a'..'z').to_a # Uses the range to create the lowercase letters and converts it to an array
+
     # Encrypts document using key
     # Arguments:
     #   document: String
@@ -7,6 +13,8 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      # This method handles the encryption of the text
+      document.to_s.chars.collect { |e| (e.ord + key).chr }.join # just add the key and transform it into chr
     end
 
     # Decrypts String document using integer key
@@ -16,9 +24,12 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      # This method is responsible for decrypting the encrypted text
+      document.to_s.chars.collect { |e| (e.ord - key).chr }.join
     end
   end
 
+  # Creating Permutation Cipher
   module Permutation
     # Encrypts document using key
     # Arguments:
@@ -27,6 +38,13 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      document = document.to_s.chars
+      generation = (0..127).to_a.shuffle(random: Random.new(key))
+      # Random.new() passing a key to shuffle the nums 0~127
+      # Since ASCII is 0~127, we can find "a" to "z" index on shuffled num on generation array by using x.ord
+      document.collect do |x|
+        generation.at(x.ord).chr
+      end.join
     end
 
     # Decrypts String document using integer key
@@ -36,6 +54,9 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      document = document.to_s.chars
+      generation = (0..127).to_a.shuffle(random: Random.new(key))
+      document.collect { |x| generation.index(x.ord).chr }.join
     end
   end
 end
